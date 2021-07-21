6e5ad25b9169d22a3bdbd0c7559bd013495bcff2
neilblaze <putubanerjee23@gmail.com>
2021-07-21 23:12:47 +0530

    navMod_pass

--- main.js
+++ main.js
@@ -2,3 +2,9 @@
 const w = 250, h = 250;
 const video = document.getElementById("video");
 [video.width, video.height] = [w, h];
+
+//WebRTC
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+// window.RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+// window.RTCSessionDescription = window.RTCSessionDescription || window.mozRTCSessionDescription || window.webkitRTCSessionDescription;
+
