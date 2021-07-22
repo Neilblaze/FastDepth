2a4b40cc50993934241ff7b48a2497809c68afb9
neilblaze <putubanerjee23@gmail.com>
2021-07-22 12:31:35 +0530

    refactor 0.2x

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //(PC/MOBILE)
-const filter = "win16|win32|win64";
+const filter = "win16|win32|win64|mac|macintel";
 const platform = (filter.indexOf(navigator.platform.toLowerCase()) > 0) ? "PC" : "MOBILE";
 console.log(`Client platform : ${platform}`);
 
@@ -10,90 +10,60 @@ const video = document.getElementById("video");
 
 //WebRTC
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-// window.RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
-// window.RTCSessionDescription = window.RTCSessionDescription || window.mozRTCSessionDescription || window.webkitRTCSessionDescription;
-
 let backlog;
-    let vidStream = null;
-    // navigator.getUserMedia({video: true}, successCallback, (err) => {
-    //     console.log("Error: ", err);
-    // });
-    let cam = "environment" // "environment" or "user"
-    const successCallback = (_vidStream) => {
-        vidStream = _vidStream;
-        // console.log("successCallback");
-        video.srcObject = _vidStream;
-        video.play();
-    }
+let vidStream = null;
+let cam = "environment" // "environment" or "user"
+const successCallback = (_vidStream) => {
+    vidStream = _vidStream;
+    video.srcObject = _vidStream;
+    video.play();
+}
 
-    const change_cam = () => {
-        if (platform == "PC") {
-            console.log("Not supported!");
-        } else {
-            if (vidStream == null) return;
-            // if (cam == "environment") {
-            //     cam = "user";
-            //     video.srcObject = vidStream;
-            //     video.play();
-            // } else {
-            //     cam = "environment";
-            //     video.srcObject = null;
-            //     video.pause();
-            // }
-            vidStream.getTracks().forEach(t => {
-                t.stop();
-            });
-            if (cam == "environment") cam = "user";
-            else cam = "environment";
-            backlog = { video: { facingMode: { exact: cam }, width: { exact: w }, height: { exact: h } }, audio: false };
-            navigator.getUserMedia(backlog, successCallback, (e) => console.log(e));
-        }
-    }
-
+const change_cam = () => {
     if (platform == "PC") {
-        backlog = { video: { width: { exact: w }, height: { exact: h } }, audio: false };
+        console.log("Not supported!");
     } else {
+        if (vidStream == null) return;
+        vidStream.getTracks().forEach(t => {
+            t.stop();
+        });
+        if (cam == "environment") cam = "user";
+        else cam = "environment";
         backlog = { video: { facingMode: { exact: cam }, width: { exact: w }, height: { exact: h } }, audio: false };
+        navigator.getUserMedia(backlog, successCallback, (e) => console.log(e));
     }
-    navigator.getUserMedia(backlog, successCallback, (e) => console.log(e));
+}
 
-    const canvas = document.getElementById("canvas");
-    [canvas.width, canvas.height] = [w, h];
-    const ctx = canvas.getContext("2d");
-    ctx.fillStyle = "rgb(0, 255, 0)";
-    // ctx.fillRect(0, 0, w, h);
-    ctx.strokeStyle = "rgb(0, 255, 0)";
-    // ctx.lineWidth = 1;
-    ctx.font = "15px Arial";
-    // ctx.textAlign = "center";
-    ctx.lineWidth = 3;
-    // ctx.strokeText("Test", w / 2, h / 2);
+if (platform == "PC") {
+    backlog = { video: { width: { exact: w }, height: { exact: h } }, audio: false };
+} else {
+    backlog = { video: { facingMode: { exact: cam }, width: { exact: w }, height: { exact: h } }, audio: false };
+}
+navigator.getUserMedia(backlog, successCallback, (e) => console.log(e));
 
+const canvas = document.getElementById("canvas");
+[canvas.width, canvas.height] = [w, h];
+const ctx = canvas.getContext("2d");
+ctx.fillStyle = "rgb(0, 255, 0)";
+ctx.strokeStyle = "rgb(0, 255, 0)";
+ctx.font = "15px Arial";
+ctx.lineWidth = 3;
 
-    let model;
-    const load = async () => {
-        const url = "./model.json";
-        // const url = "model.json";
-        model = await tf.loadGraphModel(url);
-        console.log("Loading Complete!");
-    }
-    load();
+let model;
+const load = async () => {
+    const url = "./model.json";
+    // const url = "model.json";
+    model = await tf.loadGraphModel(url);
+    console.log("Model loaded");
+}
+load();
 
-    // const predict = async () => {
-    //     const image = await tf.readImage(vidStream);
-    //     const result = await model.execute(image);
-    //     console.log(result);
-    //     const text = result.data.toString();
-    //     ctx.fillText(text, w / 2, h / 2);
-    // }
-    // predict();
-
-    const predict = async () => {
-        const image = tf.browser.fromPixels(video).resizeBilinear([224, 224]).transpose([2, 0, 1]).reshape([1, 3, 224, 224]).asType('float32').div(255);
-        result = await model.predict(image);
-        console.log("Done");
-        const outReshape = (tf.transpose(result, [2, 3, 1, 0])).reshape([224, 224, 1]);
-        const outResize = tf.mul(tf.div(outReshape, tf.max(outReshape)), 255).asType('int32');
-        await tf.browser.toPixels(outResize, canvas);
-        setTimeout(predict, 0);
-    }
+const predict = async () => {
+    const image = tf.browser.fromPixels(video).resizeBilinear([224, 224]).transpose([2, 0, 1]).reshape([1, 3, 224, 224]).asType('float32').div(255);
+    result = await model.predict(image);
+    console.log("Done");
+    const outReshape = (tf.transpose(result, [2, 3, 1, 0])).reshape([224, 224, 1]);
+    const outResize = tf.mul(tf.div(outReshape, tf.max(outReshape)), 255).asType('int32');
+    await tf.browser.toPixels(outResize, canvas);
+    setTimeout(predict, 0);
+}
